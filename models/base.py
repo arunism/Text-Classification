660104f56a7a95b132cd5b6e98280936d1e5f641
arunism <thearunism@gmail.com>
2022-05-16 19:37:47 +0545

    select optimizer

--- models/base.py
+++ models/base.py
@@ -1,4 +1,24 @@
+import torch
+
 class BaseModel:
     def __init__(self, constant, word_vectors):
         self.constant = constant
         self.word_vectors = word_vectors
+        self.vocab_size = self.constant.VOCAB_SIZE
+        self.loss = 0.0
+    
+    def get_optimizer(self):
+        if self.constant.OPTIMIZER == 'adam':
+            return torch.optim.Adam(lr=self.constant.LEARNING_RATE)
+        elif self.constant.OPTIMIZER == 'adadelta':
+            return torch.optim.Adadelta(lr=self.constant.LEARNING_RATE)
+        elif self.constant.OPTIMIZER == 'adagrad':
+            return torch.optim.Adagrad(lr=self.constant.LEARNING_RATE)
+        elif self.constant.OPTIMIZER == 'rmsprop':
+            return torch.optim.RMSprop(lr=self.constant.LEARNING_RATE)
+        elif self.constant.OPTIMIZER == 'sgd':
+            return torch.optim.SGD(lr=self.constant.LEARNING_RATE)
+        return None
+    
+    def calculate_loss(self):
+        pass
